b4e29dd2f4673e8e15e1fb810593baa951aaf3b3
Laura Smith <113145914+LSmith2174@users.noreply.github.com>
2023-05-02 01:08:00 -0400

    Multiplayer Is almost working
    Added to the multiplayer script to spawn different scenes for the server and the user and spawns in a coin on the server when a user joins

--- scene/Node2D.gd
+++ scene/Node2D.gd
@@ -5,33 +5,56 @@ func _unhandled_input(event):
 		get_tree().quit()
 		
 
-const Player = preload("res://scene/Player.tscn")
+const Player = preload("res://scene/player.tscn")
+const JoinScreen = preload("res://scene/join_screen.tscn")
+const MainPage = preload("res://scene/main_scene.tscn")
 
-@onready var main_menu = $"CanvasLayer/Main Menu"
+@onready var main_menu = $"CanvasLayer/TextureRect3"
+@onready var title = $"CanvasLayer/TextureRect2"
 @onready var main_menuBG = $CanvasLayer/TextureRect
-@onready var address_entry = $"../CanvasLayer/Main Menu/MarginContainer/VBoxContainer/RoomCode"
+#@onready var address_entry = $"../CanvasLayer/Main Menu/MarginContainer/VBoxContainer/RoomCode"
 
 const PORT = 4712
 var enet_peer = ENetMultiplayerPeer.new()
 
 func _on_host_butt_pressed():
 	main_menu.hide()
+	title.hide()
 	main_menuBG.hide()
 	
-	enet_peer.create_server(PORT)
+	enet_peer.create_server(PORT, 5)
 	multiplayer.multiplayer_peer = enet_peer
+	multiplayer.peer_connected.connect(add_player)
 	
-	add_player(multiplayer.get_unique_id())
+	add_board(multiplayer.get_unique_id())
 
 
 func _on_join_butt_pressed():
 	main_menu.hide()
+	title.hide()
 	main_menuBG.hide()
 	
+	enet_peer.create_client("localhost", PORT)
+	multiplayer.multiplayer_peer = enet_peer
+	
+	join_board(multiplayer.get_unique_id())
+	
+func join_board(peer_id):
+	var screen = JoinScreen.instantiate()
+	screen.name = str(peer_id)
+	add_child(screen)
+
 func add_player(peer_id):
 	var player = Player.instantiate()
 	player.name = str(peer_id)
+	player.translate(Vector2(487, 875))
 	add_child(player)
+
+func add_board(peer_id):
+	var board = MainPage.instantiate()
+	
+	board.name = str(peer_id)
+	add_child(board)
 
 
 #func _on_roll_butt_pressed():
